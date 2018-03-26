6b0ecd12f82900f7b1e42d4ffbeb09ac5a5950c2
merrow0 <merrow0@gmail.com>
2018-03-26 18:40:44 +0200

    init

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    public Vector3 moveToPos;
+    public float movementSpeed;
+
+    Rigidbody2D rigid;
+
+	void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+
+        moveToPos = transform.position;
+    }
+	
+	void Update()
+    {
+	}
+
+    void FixedUpdate()
+    {
+        Vector3 heading = moveToPos - transform.position;
+        float distance = heading.magnitude;
+
+        if (distance < 0.1f)
+        {
+            rigid.MovePosition(moveToPos);
+            rigid.velocity = Vector3.zero;
+        }
+        else
+        {
+            Vector3 normalizedDirection = heading / distance;
+            rigid.velocity = normalizedDirection * movementSpeed;
+        }
+    }
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject selectedCharater;
+
+	void Start ()
+    {
+	    	
+	}
+	
+	void Update ()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 rayPos = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
+            RaycastHit2D hit = Physics2D.Raycast(rayPos, Vector2.zero, 0f);
+
+            if (hit.collider)
+            {
+                if (hit.collider.tag == "Player")
+                {
+                    selectedCharater = hit.collider.gameObject;
+                }
+            }
+            else
+            {
+                if (selectedCharater)
+                {
+                    selectedCharater.GetComponent<Character>().moveToPos = rayPos;
+                }
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+            selectedCharater = GameObject.Find("Tank");
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+            selectedCharater = GameObject.Find("Healer");
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+            selectedCharater = GameObject.Find("DamageDealer1");
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+            selectedCharater = GameObject.Find("DamageDealer2");
+
+    }
+}
